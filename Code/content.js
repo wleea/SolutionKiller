55c19234d56d7bda8f89610f1f6259078b3fd91d
wleea <124417910+wleea@users.noreply.github.com>
2024-11-09 16:00:40 +0800

    Update content.js

--- Code/content.js
+++ Code/content.js
@@ -2,6 +2,13 @@
 if (window.location.href.includes("luogu.com.cn/problem/solution") || 
     (window.location.href.includes("/p/") && window.location.href.includes("/solution"))) {
     
-    // 使用 chrome.tabs.update 重定向到提示页面
-    chrome.runtime.sendMessage({ action: "redirectToMessagePage" });
+    // 更新题解打开次数
+    chrome.storage.local.get(['solutionCount'], (result) => {
+        let count = result.solutionCount || 0;
+        count++;
+        chrome.storage.local.set({ solutionCount: count }, () => {
+            // 发送消息以重定向到提示页面
+            chrome.runtime.sendMessage({ action: "redirectToMessagePage" });
+        });
+    });
 }
