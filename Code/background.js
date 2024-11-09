5eaadf598b3137883a051ecab06a3cb3aff7d70f
wleea <124417910+wleea@users.noreply.github.com>
2024-11-09 16:41:38 +0800

    Update background.js

--- Code/background.js
+++ Code/background.js
@@ -1,10 +1,13 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "resetCount") {
+    if (request.action === "redirectToMessagePage") {
+        // 打开提示页面
+        chrome.tabs.update(sender.tab.id, { url: chrome.runtime.getURL("message.html") });
+    } else if (request.action === "resetCount") {
         // 清零计数
         chrome.storage.local.set({ solutionCount: 0 }, () => {
             console.log("题解打开次数已清零");
             sendResponse({ success: true }); // 发送响应
         });
-        return true; // 表示将保持消息通道打开，直到 sendResponse 被调用
+        return true; // 保持消息通道打开
     }
 });
