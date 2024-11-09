889f9b2f82e0a58757683087f2d76cdaaa7dede4
wleea <124417910+wleea@users.noreply.github.com>
2024-11-09 16:00:14 +0800

    Update content.js

--- Code/content.js
+++ Code/content.js
@@ -2,6 +2,6 @@
 if (window.location.href.includes("luogu.com.cn/problem/solution") || 
     (window.location.href.includes("/p/") && window.location.href.includes("/solution"))) {
     
-    // 获取当前标签页的 ID
-    chrome.runtime.sendMessage({action: "redirectToMessagePage"});
+    // 使用 chrome.tabs.update 重定向到提示页面
+    chrome.runtime.sendMessage({ action: "redirectToMessagePage" });
 }
